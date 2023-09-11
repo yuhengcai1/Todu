9fe7c8c59d34a3472c359e21b4c851fe95badfe2
yuhengcai1 <cai99@wisc.edu>
2023-09-11 13:02:38 -0400

    Initial Commit

---
+++ Todu/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Todu
+//
+//  Created by 蔡雨衡 on 9/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Todu/ToduApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToduApp.swift
+//  Todu
+//
+//  Created by 蔡雨衡 on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ToduApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
