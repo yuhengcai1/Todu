67b8fa2b5937bd6dcf83f9c795191b1cce866a63
yuhengcai1 <cai99@wisc.edu>
2023-09-11 14:34:37 -0400

    Start

--- Todu/ContentView.swift
+++ Todu/ContentView.swift
@@ -7,16 +7,77 @@
 
 import SwiftUI
 
+struct Havefun: Identifiable {
+  let id    = UUID()
+  var isOn  = true
+  let Fun: String
+}
+
 struct ContentView: View {
+    
+
+    
+    @StateObject private var viewModel = FunViewModel()
+    
+    @State private var newFun: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                TextField("GET NEW ONE", text: $newFun)
+                Button("Confirm") {
+                    let newItem = Havefun(Fun: newFun)
+                    viewModel.append(newItem)
+                    newFun = ""
+                }
+            }
+            .padding()
+            List {
+                ForEach(viewModel.More) { item in
+                    HStack{
+                        Text(item.Fun).foregroundColor(item.isOn ? .blue : .gray)
+                        Spacer()
+                        Group {
+                                      if item.isOn {
+                                        Image(systemName: "circle")
+                                      }
+                                      else {
+                                        Image(systemName: "heart.fill")
+                                      }
+                    }
+                        .foregroundColor(.gray)
+                        .onTapGesture {
+                                      viewModel.toggle(item)
+                        }
+                    }
+                    
+                }
+            }
         }
-        .padding()
     }
+}
+
+
+class FunViewModel: ObservableObject {
+  @Published private(set) var More: [Havefun]
+
+  init() {
+    self.More = [
+        Havefun(Fun: "Yuheng"),
+        Havefun(Fun: "Cai"),
+        Havefun(Fun: "Apple")
+    ]
+  }
+
+  func append(_ item: Havefun) {
+    More.append(item)
+  }
+
+  func toggle(_ item: Havefun) {
+    if let index = More.firstIndex(where: {$0.id == item.id}) {
+      More[index].isOn.toggle()
+    }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -24,3 +85,6 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
